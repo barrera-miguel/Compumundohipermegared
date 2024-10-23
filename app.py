d3d06dadc5c552cf684643fbdb71d81083332876
Matias <mercadoroque7@gmail.com>
2024-10-23 13:47:28 -0300

    Funcion de Pronostico Extendido

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ while True:
         else:
             break
     
-    API_KEY ="acá va la api key"
+    API_KEY = "a44420d695aa26daccb827e71f32aaa5"
     url = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={API_KEY}&units=metric&lang=es"
     res = requests.get(url)
     data = res.json()
@@ -35,6 +35,29 @@ while True:
     print(f"Humedad: {humedad} %")
     print(f"Descripción: {descripcion}")
     print("-----------------------------")
+
+    while True:
+        opcion_pronostico = input("¿Quiere ver el pronóstico extendido a 5 días? (ingrese 's' para sí o 'n' para no): ").lower()
+        print("-----------------------------")
+
+        if opcion_pronostico == 'n':
+            break
+        elif opcion_pronostico == 's':
+            url_forecast = f"https://api.openweathermap.org/data/2.5/forecast?q={ciudad}&appid={API_KEY}&units=metric&lang=es"
+            res_forecast = requests.get(url_forecast)
+            data_forecast = res_forecast.json()
+
+            print("-------Pronóstico Extendido a 5 Días-------")
+            for forecast in data_forecast['list']:
+                fecha = forecast['dt_txt']
+                temp_forecast = forecast['main']['temp']
+                desc_forecast = forecast['weather'][0]['description']
+                print(f"{fecha}: {temp_forecast}°C, {desc_forecast}")
+            print("------------------------------------------")
+            break
+        else:
+            print("Opción no válida. Intente nuevamente")
+            print("-----------------------------")
 
     while True:
         opcion = input("¿Quiere hacer otra consulta? (ingrese 's' para sí o 'n' para no): ").lower()
