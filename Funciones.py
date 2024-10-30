d331b3d13ecc72e6c6679ae9d2f1c8aedc21cbde
Federico-Frankenberger <fafrankenberger@gmail.com>
2024-10-30 20:57:29 -0300

    Menú-DockerCompose

---
+++ Funciones.py
@@ -0,0 +1,115 @@
+import requests
+def mostrar_portada():
+    print("-----------------------------")
+    print("--------APP DEL CLIMA--------")
+    print("by Compumundohipermegared")
+    print("-----------------------------")
+
+def seleccionar_unidad():
+    while True:
+        unidad = input("Unidad de temperatura (C para Celsius, F para Fahrenheit): ").strip().lower()
+        if unidad == 'c':
+            return 'metric', '°C'
+        elif unidad == 'f':
+            return 'imperial', '°F'
+        else:
+            print("-----------------------------")
+            print("Entrada no válida. Intente nuevamente")
+            print("-----------------------------")
+
+def unidad_actual(unidad):
+    if unidad == "metric":
+        return"Celcius"
+    elif unidad == "imperial":
+        return"Fahrenheit"
+    
+def cambio_unidad(unidad,simbolo):
+    while True:
+        print("------Medidas------")
+        print(f"Actual: " + unidad_actual(unidad))
+        seleccion = input ("a- Celcius\nb- Fahrenheit\nc- Volver\n")
+        if seleccion.lower() =="a":
+            unidad ='metric', '°C'
+            return unidad
+        elif seleccion.lower() == "b":
+            unidad = 'imperial', '°F'
+            return unidad
+        elif seleccion.lower() =="c":
+            return unidad,simbolo
+        else:
+            print("---------------------------ERROR------------------------------")
+            print("Opción no válida. Por favor, seleccione nuevamente a - b - c .")
+            print("--------------------------------------------------------------")
+
+    
+def ingresar_ciudad():
+    while True:
+        ciudad = input("Ingrese una ciudad: ").strip().title()
+        if ciudad.isdigit() or len(ciudad) == 0:
+            print("-----------------------------")
+            print("Entrada no válida. Intente nuevamente")
+            print("-----------------------------")
+        else:
+            return ciudad
+
+def solicitar_clima(ciudad, API_KEY, units):
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={API_KEY}&units={units}&lang=es"
+    res = requests.get(url)
+    return res.json()
+
+def mostrar_clima_actual(data, ciudad, simbolo):
+    temp = data["main"]["temp"]
+    descripcion = data["weather"] [0] ["description"]
+    minima = data["main"]["temp_min"]
+    maxima = data["main"]["temp_max"]
+    humedad = data["main"]["humidity"]
+    print("-----------------------------")
+    print(f'Temperatura actual en {ciudad}: {temp} {simbolo}')
+    print(f'Temperatura máxima: {maxima} {simbolo}')
+    print(f'Temperatura mínima: {minima} {simbolo}')
+    print(f"Humedad: {humedad} %")
+    print(f"Descripción: {descripcion}")
+    print("-----------------------------")
+
+def solicitar_clima_extendido(ciudad, API_KEY, units):
+    url_forecast = f"https://api.openweathermap.org/data/2.5/forecast?q={ciudad}&appid={API_KEY}&units={units}&lang=es"
+    res_forecast = requests.get(url_forecast)
+    return res_forecast.json()
+
+def mostrar_pronostico_extendido(data_forecast, simbolo):
+    print("-------Pronóstico Extendido a 5 Días-------")
+    for forecast in data_forecast['list']:
+        fecha = forecast['dt_txt']
+        temp_forecast = forecast['main']['temp']
+        hum_forecast = forecast["main"]["humidity"]
+        desc_forecast = forecast['weather'][0]['description']
+        print(f"{fecha}: {temp_forecast} {simbolo}, Humedad: {hum_forecast} %, {desc_forecast}")
+    print("------------------------------------------")
+
+def historial():
+    while True:
+            print("------HISTORIAL------")
+            historial = input("a- Diario\nb- Extendido\nc- Volver\n")
+            if historial.lower() == "a":
+                print("--Muestro historial diario--")
+            elif historial.lower() =="b":
+                print("--Muestro historial extendido--")
+            elif historial.lower() == "c":
+                break
+            else:
+                print("---------------------------ERROR------------------------------")
+                print("Opción no válida. Por favor, seleccione nuevamente a - b - c .")
+                print("--------------------------------------------------------------")
+
+def configuracion():
+    while True:
+            print("------CONFIGURACIÓN------")
+            config = input("a- Unidad de medida\nb- Volver\n")
+            if config.lower() == "a":
+                units,simbolo = cambio_unidad(units,simbolo)
+            elif config.lower() == "b":
+                break
+            else: 
+                print("---------------------------ERROR--------------------------")
+                print("Opción no válida. Por favor, seleccione nuevamente a - b .")
+                print("----------------------------------------------------------")
--- app.py
+++ app.py
@@ -1,106 +1,31 @@
-import requests
 from dotenv import load_dotenv
+from Funciones import mostrar_portada,seleccionar_unidad,ingresar_ciudad,solicitar_clima,mostrar_clima_actual,solicitar_clima_extendido,mostrar_pronostico_extendido,historial,configuracion
 import os
-
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
-
-def mostrar_portada():
-    print("-----------------------------")
-    print("--------APP DEL CLIMA--------")
-    print("by Compumundohipermegared")
-    print("-----------------------------")
-
-def seleccionar_unidad():
-    while True:
-        unidad = input("Selecciona la unidad  de temperatura (C para Celsius, F para Fahrenheit): ").strip().lower()
-        if unidad == 'c':
-            return 'metric', '°C'
-        elif unidad == 'f':
-            return 'imperial', '°F'
-        else:
-            print("-----------------------------")
-            print("Entrada no válida. Intente nuevamente")
-            print("-----------------------------")
-        
-def ingresar_ciudad():
-    while True:
-        ciudad = input("Ingrese una ciudad: ").strip().title()
-        if ciudad.isdigit() or len(ciudad) == 0:
-            print("-----------------------------")
-            print("Entrada no válida. Intente nuevamente")
-            print("-----------------------------")
-        else:
-            return ciudad
-
-def solicitar_clima(ciudad, API_KEY, units):
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={ciudad}&appid={API_KEY}&units={units}&lang=es"
-    res = requests.get(url)
-    return res.json()
-
-def mostrar_clima_actual(data, ciudad, simbolo):
-    temp = data["main"]["temp"]
-    descripcion = data["weather"] [0] ["description"]
-    minima = data["main"]["temp_min"]
-    maxima = data["main"]["temp_max"]
-    humedad = data["main"]["humidity"]
-    print("-----------------------------")
-    print(f'Temperatura actual en {ciudad}: {temp} {simbolo}')
-    print(f'Temperatura máxima: {maxima} {simbolo}')
-    print(f'Temperatura mínima: {minima} {simbolo}')
-    print(f"Humedad: {humedad} %")
-    print(f"Descripción: {descripcion}")
-    print("-----------------------------")
-
-def preguntar_pronostico():
-    while True:
-        opcion = input("¿Quiere ver el pronóstico extendido a 5 días? (ingrese 's' para sí o 'n' para no): ").strip().lower()
-        if opcion in ('s', 'n'):
-            return opcion == 's'
-        print("-----------------------------")
-        print("Opción no válida. Intente nuevamente")
-        print("-----------------------------")
-
-def solicitar_clima_extendido(ciudad, API_KEY, units):
-    url_forecast = f"https://api.openweathermap.org/data/2.5/forecast?q={ciudad}&appid={API_KEY}&units={units}&lang=es"
-    res_forecast = requests.get(url_forecast)
-    return res_forecast.json()
-
-def mostrar_pronostico_extendido(data_forecast, simbolo):
-    print("-------Pronóstico Extendido a 5 Días-------")
-    for forecast in data_forecast['list']:
-        fecha = forecast['dt_txt']
-        temp_forecast = forecast['main']['temp']
-        hum_forecast = forecast["main"]["humidity"]
-        desc_forecast = forecast['weather'][0]['description']
-        print(f"{fecha}: {temp_forecast} {simbolo}, Humedad: {hum_forecast} %, {desc_forecast}")
-    print("------------------------------------------")
-
-def realizar_nueva_consulta():
-    while True:
-        opcion = input("¿Quiere hacer otra consulta? (ingrese 's' para sí o 'n' para no): ").lower()
-        if opcion in ('s', 'n'):
-            return opcion == 's'
-        print("-----------------------------")
-        print("Opción no válida. Intente nuevamente")
-        print("-----------------------------")
-
-def main():
-    mostrar_portada()
-
-    while True:
-        units, simbolo = seleccionar_unidad()
-        ciudad = ingresar_ciudad()
-        dataClima = solicitar_clima(ciudad, API_KEY, units)
-        mostrar_clima_actual(dataClima, ciudad, simbolo)
-
-        if preguntar_pronostico():
-            dataPronostico = solicitar_clima_extendido(ciudad, API_KEY, units)
-            mostrar_pronostico_extendido(dataPronostico, simbolo)
-
-        if not realizar_nueva_consulta():
+mostrar_portada()
+units, simbolo = seleccionar_unidad()
+ciudad=""
+while True:
+    print("-----------MENÚ-----------\n 1- Pronóstico actual \n 2- Pronóstico extendido \n 3- Historial de consultas\n 4- Configuración \n 5- Salir ")
+    seleccion = input("Seleccione una opción (1-5): ")
+    if seleccion == "1":
+       mostrar_clima_actual(solicitar_clima(ingresar_ciudad(), API_KEY, units), ciudad, simbolo)
+    elif seleccion == "2":
+        mostrar_pronostico_extendido(solicitar_clima_extendido(ingresar_ciudad(), API_KEY, units), simbolo)
+    elif seleccion == "3":
+        historial()
+    elif seleccion == "4":
+        configuracion()
+    elif seleccion == "5":
+        continuar = input("¿Está seguro de querer salir? (s/n): ")
+        if continuar.lower() != 'n':
             print("Saliendo...")
             break
+        else:print("Regresando al menú...")
+    else:
+        print("---------------------------ERROR-------------------------------")
+        print("Opción no válida. Por favor, seleccione nuevamente entre 1 y 5.")
+        print("---------------------------------------------------------------")
+           
 
-if __name__ == "__main__":
-    main()
